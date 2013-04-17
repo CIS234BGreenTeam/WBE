20af6029bcbf4a1e3ca77bf3790de5e1184091b6
Kris <kris.frye@gmail.com>
2013-04-16 19:52:34 -0700

    Created validation and unit tests for Customer
    Also deleted some auto-generated unit test files that I don't think we
    will be using.

--- Web/WBE-WebApplication/TestWBE/CustomerTest.vb
+++ Web/WBE-WebApplication/TestWBE/CustomerTest.vb
@@ -4,15 +4,12 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 Imports libWBEBR
 
-
-
 '''<summary>
 '''This is a test class for CustomerTest and is intended
 '''to contain all CustomerTest Unit Tests
 '''</summary>
 <TestClass()> _
 Public Class CustomerTest
-
 
     Private testContextInstance As TestContext
 
@@ -55,7 +52,6 @@ Public Class CustomerTest
     '
 #End Region
 
-
     '''<summary>
     '''A test for Customer Constructor
     '''</summary>
@@ -66,156 +62,361 @@ Public Class CustomerTest
     End Sub
 
     '''<summary>
-    '''A test for Address1
+    '''A test for Address1 with a correct address length
     '''</summary>
     <TestMethod()> _
-    Public Sub Address1Test()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    Public Sub Address1CorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "123 Main Street"
         Dim actual As String
         target.Address1 = expected
         actual = target.Address1
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
-
+    '''<summary>
+    '''A test for Address1 with a incorrect address length
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "Address must be less than 40 characters in length.")>
+    Public Sub Address1IncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "12345678901234567890123456789012345678901"
+        Dim actual As String
+        target.Address1 = expected
+        actual = target.Address1
+    End Sub
     '''<summary>
     '''A test for Address2
     '''</summary>
     <TestMethod()> _
-    Public Sub Address2Test()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    Public Sub Address2CorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "123 Main Street"
         Dim actual As String
         target.Address2 = expected
         actual = target.Address2
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for City
+    '''A test for Address2 with a incorrect address length
     '''</summary>
     <TestMethod()> _
-    Public Sub CityTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    <ExpectedException(GetType(System.Exception), "Address must be less than 40 characters in length.")>
+    Public Sub Address2IncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "12345678901234567890123456789012345678901"
+        Dim actual As String
+        target.Address2 = expected
+        actual = target.Address2
+    End Sub
+
+    '''<summary>
+    '''A test for City using correct length
+    '''</summary>
+    <TestMethod()> _
+    Public Sub CityCorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "Portland"
         Dim actual As String
         target.City = expected
         actual = target.City
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for Email
+    '''A test for City using incorrect length
     '''</summary>
     <TestMethod()> _
-    Public Sub EmailTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    <ExpectedException(GetType(System.Exception), "City must be less than 25 characters is length.")>
+    Public Sub CityIncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "12345678901234567890123456"
+        Dim actual As String
+        target.City = expected
+        actual = target.City
+    End Sub
+    '''<summary>
+    '''A test for Email with correct format
+    '''</summary>
+    <TestMethod()> _
+    Public Sub EmailCorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "test@test.com"
         Dim actual As String
         target.Email = expected
         actual = target.Email
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
+    End Sub
+    '''<summary>
+    '''A test for Email with incorrect with no .com
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "Email should use the format: 'name@domain.com' or similar")>
+    Public Sub EmailIncorrectNoEndTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "test@test"
+        Dim actual As String
+        target.Email = expected
+        actual = target.Email
     End Sub
 
     '''<summary>
-    '''A test for Fax
+    '''A test for Email with incorrect with no name
     '''</summary>
     <TestMethod()> _
-    Public Sub FaxTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    <ExpectedException(GetType(System.Exception), "Email should use the format: 'name@domain.com' or similar")>
+    Public Sub EmailIncorrectNoNameTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "@test.com"
+        Dim actual As String
+        target.Email = expected
+        actual = target.Email
+    End Sub
+
+    '''<summary>
+    '''A test for Email with incorrect with no @
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "Email should use the format: 'name@domain.com' or similar")>
+    Public Sub EmailIncorrectNoAtTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "test.test.com"
+        Dim actual As String
+        target.Email = expected
+        actual = target.Email
+    End Sub
+    '''<summary>
+    '''A test for Fax using the format ###-###-####
+    '''</summary>
+    <TestMethod()> _
+    Public Sub FaxCorrectNoParenthesisTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "503-111-2222"
         Dim actual As String
         target.Fax = expected
         actual = target.Fax
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for Name
+    '''A test for Fax using the format (###) ###-####
     '''</summary>
     <TestMethod()> _
-    Public Sub NameTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    Public Sub FaxCorrectParenthesisTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "(503) 111-2222"
+        Dim actual As String
+        target.Fax = expected
+        actual = target.Fax
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Fax using the format (###)###-####
+    '''</summary>
+    <TestMethod()> _
+    Public Sub FaxCorrectParenthesisNoSpaceTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "(503)111-2222"
+        Dim actual As String
+        target.Fax = expected
+        actual = target.Fax
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Fax using the format ##########
+    '''</summary>
+    <TestMethod()> _
+    Public Sub FaxCorrectNoSymbolsTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "5031112222"
+        Dim actual As String
+        target.Fax = expected
+        actual = target.Fax
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Fax using an incorrect number of numbers
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "Fax should be in the format '(###) ###-####' or '###-###-####'")>
+    Public Sub FaxIncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "503111222"
+        Dim actual As String
+        target.Fax = expected
+        actual = target.Fax
+    End Sub
+
+    '''<summary>
+    '''A test for Name using 25 characters (max length)
+    '''</summary>
+    <TestMethod()> _
+    Public Sub NameTest25Chars()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "testname90123456789012345"
         Dim actual As String
         target.Name = expected
         actual = target.Name
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for Order
+    '''A test for Name using 26 characters (max length+1)
     '''</summary>
     <TestMethod()> _
-    Public Sub OrderTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As List(Of Order) = Nothing ' TODO: Initialize to an appropriate value
-        Dim actual As List(Of Order)
-        target.Order = expected
-        actual = target.Order
-        Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
+    <ExpectedException(GetType(System.Exception), "Name must be no more than 25 characters in length.")>
+    Public Sub NameTest26Chars()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "testname901234567890123456"
+        Dim actual As String
+        target.Name = expected
+        actual = target.Name
     End Sub
 
     '''<summary>
-    '''A test for Phone
+    '''A test for Phone using the format ###-###-####
     '''</summary>
     <TestMethod()> _
-    Public Sub PhoneTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    Public Sub PhoneCorrectNoParenthesisTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "503-111-2222"
         Dim actual As String
         target.Phone = expected
         actual = target.Phone
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for State
+    '''A test for Phone using the format (###) ###-####
     '''</summary>
     <TestMethod()> _
-    Public Sub StateTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As String = String.Empty ' TODO: Initialize to an appropriate value
+    Public Sub PhoneCorrectParenthesisTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "(503) 111-2222"
+        Dim actual As String
+        target.Phone = expected
+        actual = target.Phone
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Phone using the format (###)###-####
+    '''</summary>
+    <TestMethod()> _
+    Public Sub PhoneCorrectParenthesisNoSpaceTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "(503)111-2222"
+        Dim actual As String
+        target.Phone = expected
+        actual = target.Phone
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Phone using the format ##########
+    '''</summary>
+    <TestMethod()> _
+    Public Sub PhoneCorrectNoSymbolsTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "5031112222"
+        Dim actual As String
+        target.Phone = expected
+        actual = target.Phone
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Phone using an incorrect number of numbers
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "Phone should be in the format '(###) ###-####' or '###-###-####'")>
+    Public Sub PhoneIncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "503111222"
+        Dim actual As String
+        target.Phone = expected
+        actual = target.Phone
+    End Sub
+
+    '''<summary>
+    '''A test for State using 2 correct letters
+    '''</summary>
+    <TestMethod()> _
+    Public Sub StateCorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "OR"
         Dim actual As String
         target.State = expected
         actual = target.State
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
     End Sub
 
     '''<summary>
-    '''A test for Stock
+    '''A test for State using 3 incorrect letters
     '''</summary>
     <TestMethod()> _
-    Public Sub StockTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As List(Of BakedGood) = Nothing ' TODO: Initialize to an appropriate value
-        Dim actual As List(Of BakedGood)
-        target.Stock = expected
-        actual = target.Stock
-        Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
+    <ExpectedException(GetType(System.Exception), "Please use a correct state abbreviation.")>
+    Public Sub StateIncorrectTest()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "ORG"
+        Dim actual As String
+        target.State = expected
+        actual = target.State
     End Sub
 
     '''<summary>
-    '''A test for Zip
+    '''A test for Zip using correct 5 digit format
     '''</summary>
     <TestMethod()> _
-    Public Sub ZipTest()
-        Dim target As Customer = New Customer() ' TODO: Initialize to an appropriate value
-        Dim expected As Integer = 0 ' TODO: Initialize to an appropriate value
-        Dim actual As Integer
+    Public Sub ZipTest5DigitFormat()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "12345"
+        Dim actual As String
         target.Zip = expected
         actual = target.Zip
         Assert.AreEqual(expected, actual)
-        Assert.Inconclusive("Verify the correctness of this test method.")
+    End Sub
+
+    '''<summary>
+    '''A test for Zip using correct 9 digit format
+    '''</summary>
+    <TestMethod()> _
+    Public Sub ZipTest9DigitFormat()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "12345-6789"
+        Dim actual As String
+        target.Zip = expected
+        actual = target.Zip
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    '''<summary>
+    '''A test for Zip using incorrect 9 digit format
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "The Zip needs to be in the format '#####' or '#####-####'")>
+    Public Sub ZipTestIncorrect9DigitFormat()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "123456789"
+        Dim actual As String
+        target.Zip = expected
+        actual = target.Zip
+    End Sub
+
+    '''<summary>
+    '''A test for Zip using incorrect format with 6 digits
+    '''</summary>
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception), "The Zip needs to be in the format '#####' or '#####-####'")>
+    Public Sub ZipTestIncorrect6DigitFormat()
+        Dim target As Customer = New Customer()
+        Dim expected As String = "123456"
+        Dim actual As String
+        target.Zip = expected
+        actual = target.Zip
     End Sub
 End Class
--- Web/WBE-WebApplication/libWBEBR/Customer.vb
+++ Web/WBE-WebApplication/libWBEBR/Customer.vb
@@ -1,13 +1,213 @@
-﻿Public Class Customer
-    Public Property Name() As String
-    Public Property Address1() As String
-    Public Property Address2() As String
-    Public Property City As String
-    Public Property State As String
-    Public Property Zip As Integer
-    Public Property Phone As String
-    Public Property Fax As String
-    Public Property Email As String
+﻿Imports System.Text.RegularExpressions
+
+Public Class Customer
     Public Property Stock As List(Of BakedGood)
     Public Property Order As List(Of Order)
+
+    Const c_NameMaxLength As Integer = 25
+    Private _Name As String
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(ByVal value As String)
+            If value.Length > c_NameMaxLength Then
+                Throw New Exception(NameError)
+            Else
+                _Name = value
+            End If
+        End Set
+    End Property
+    Private _NameError As String = String.Format("Name must be no more than {0} characters in length.", c_NameMaxLength)
+    Public ReadOnly Property NameError() As String
+        Get
+            Return _NameError
+        End Get
+    End Property
+
+    Private Const c_AddressMaxLength As Integer = 40
+    Private _Address1 As String
+    Public Property Address1() As String
+        Get
+            Return _Address1
+        End Get
+        Set(ByVal value As String)
+            If value.Length > c_AddressMaxLength Then
+                Throw New Exception(AddressError)
+            Else
+                _Address1 = value
+            End If
+            _Address1 = value
+        End Set
+    End Property
+
+    Private _Address2 As String
+    Public Property Address2() As String
+        Get
+            Return _Address2
+        End Get
+        Set(ByVal value As String)
+            If value.Length > c_AddressMaxLength Then
+                Throw New Exception(AddressError)
+            Else
+                _Address2 = value
+            End If
+        End Set
+    End Property
+    Private _AddressError As String = String.Format("Address must be less than {0} characters in length.", c_AddressMaxLength)
+    Public ReadOnly Property AddressError() As String
+        Get
+            Return _AddressError
+        End Get
+    End Property
+
+    Private Const c_CityMaxLength As Integer = 25
+    Private _City As String
+    Public Property City() As String
+        Get
+            Return _City
+        End Get
+        Set(ByVal value As String)
+            If value.Length > c_CityMaxLength Then
+                Throw New Exception(CityError)
+            Else
+                _City = value
+            End If
+        End Set
+    End Property
+
+    Private _CityError As String = String.Format("City must be less than {0} characters is length.", c_CityMaxLength)
+    Public ReadOnly Property CityError As String
+        Get
+            Return _CityError
+        End Get
+    End Property
+    Private _State As String
+    Public Property State() As String
+        Get
+            Return _State
+        End Get
+        Set(ByVal value As String)
+            Dim StateMatch As Match
+            StateMatch = Regex.Match(value.ToUpper, "^[A-Z]{2}")
+            If value.Length = 2 And StateMatch.Success Then
+                _State = value
+            Else
+                Throw New Exception(StateError)
+            End If
+        End Set
+    End Property
+
+    Private _StateError As String = "Please use a correct state abbreviation."
+    Public ReadOnly Property StateError() As String
+        Get
+            Return _StateError
+        End Get
+    End Property
+
+    Private _Zip As String
+    Public Property Zip() As String
+        Get
+            Return _Zip
+        End Get
+        Set(ByVal value As String)
+            Dim ZipMatch As Match
+            If value.Length = 5 Then
+                ZipMatch = Regex.Match(value, "^[0-9]{5}")
+            Else
+                ZipMatch = Regex.Match(value, "^[0-9]{5}-[0-9]{4}")
+            End If
+
+            If ZipMatch.Success Then
+                _Zip = value
+            Else
+                Throw New Exception(ZipError)
+            End If
+        End Set
+    End Property
+
+    Private _ZipError As String = "The Zip needs to be in the format '#####' or '#####-####'"
+    Public ReadOnly Property ZipError() As String
+        Get
+            Return _ZipError
+        End Get
+    End Property
+
+    Private _Phone As String
+    Public Property Phone() As String
+        Get
+            Return _Phone
+        End Get
+        Set(ByVal value As String)
+            If CheckPhoneFormat(value) Then
+                _Phone = value
+            Else
+                Throw New Exception(PhoneError)
+            End If
+        End Set
+    End Property
+    Private _PhoneError As String = "Phone should be in the format '(###) ###-####' or '###-###-####'"
+    Public ReadOnly Property PhoneError() As String
+        Get
+            Return _PhoneError
+        End Get
+    End Property
+
+    Private _Fax As String
+    Public Property Fax() As String
+        Get
+            Return _Fax
+        End Get
+        Set(ByVal value As String)
+            If CheckPhoneFormat(value) Then
+                _Fax = value
+            Else
+                Throw New Exception(FaxError)
+            End If
+        End Set
+    End Property
+
+    Private _FaxError As String = "Fax should be in the format '(###) ###-####' or '###-###-####'"
+    Public ReadOnly Property FaxError() As String
+        Get
+            Return _FaxError
+        End Get
+    End Property
+    Private _Email As String
+    Public Property Email() As String
+        Get
+            Return _Email
+        End Get
+        Set(ByVal value As String)
+            Dim EmailMatch As Match
+            EmailMatch = Regex.Match(value.ToUpper, "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$")
+            If EmailMatch.Success Then
+                _Email = value
+            Else
+                Throw New Exception(EmailError)
+            End If
+        End Set
+    End Property
+    Private _EmailError As String = "Email should use the format: 'name@domain.com' or similar"
+    Public ReadOnly Property EmailError() As String
+        Get
+            Return _EmailError
+        End Get
+    End Property
+
+    Public Function CheckPhoneFormat(ByVal value As String) As Boolean
+        Dim PhoneMatch As Match
+
+        If value.Length = 10 Then
+            PhoneMatch = Regex.Match(value, "^[0-9]{10}")
+        ElseIf value.Length = 12 Then
+            PhoneMatch = Regex.Match(value, "^[0-9]{3}-[0-9]{3}-[0-9]{4}")
+        ElseIf value.Length = 14 Then
+            PhoneMatch = Regex.Match(value, "^\([0-9]{3}\) [0-9]{3}-[0-9]{4}")
+        Else
+            PhoneMatch = Regex.Match(value, "^\([0-9]{3}\)[0-9]{3}-[0-9]{4}")
+        End If
+
+        Return PhoneMatch.Success
+    End Function
 End Class
